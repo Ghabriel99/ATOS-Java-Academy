2572806c805fe12066941bd5f69c4eb645b0faa4
Ghabriel Marchesi Schiavo <86672683+Ghabriel99@users.noreply.github.com>
2023-04-12 22:00:13 -0300

    .
    .

--- Java_Aula_1204/Java/src/Aula_1204/Principal.java
+++ Java_Aula_1204/Java/src/Aula_1204/Principal.java
@@ -3,16 +3,16 @@ package Aula_1204;
 public class Principal {
 
    public static void main(String[] args) {
-      System.out.println("A classe pessoa foi instanciada");
-      
-      Pessoa pessoa = new Pessoa();
+      // System.out.println("A classe pessoa foi instanciada");
+      // Pessoa pessoa = new Pessoa();
       // pessoa.idade = 33;
+      // System.out.println(pessoa.idade);
 
-      System.out.println(pessoa.idade);
+      Carro carro  = new Carro();
+      carro.ligar();
+      carro.desligar();
+      carro.exibirDados();
 
-      
-      
-      
    }
    
 }
